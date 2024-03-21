80e35ae5bd8da7976b97779c69246eed42196253
himanxu1 <technophilic000@gmail.com>
2024-03-21 15:57:30 +0530

    added-admin-btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,14 @@ const Navbar = () => {
         {
           hastoken ? <><p className='mr-10 font-mono '><Link to='/wishlist' className='flex items-center hover:text-slate-300'>Wishlist<BsBookmarkHeartFill className='ml-2 text-xl'/></Link></p>
           <p className='mr-10 cursor-pointer font-mono hover:text-slate-300'><Link to='/products'>All Products</Link></p>
-          <p><Link to='/cart'  className='hover:text-slate-300'>CART(<span className='font-mono text-red-300 '>{cartItem.length}</span>)</Link></p> </> :null
+         
+          <p><Link to='/cart'  className='hover:text-slate-300'>CART(<span className='font-mono text-red-300 '>{cartItem.length}</span>)</Link></p>
+          <p className='ml-10 cursor-pointer text-red-400 font-mono hover:text-yellow-300'><a href="https://full-stack-ecom-36jr.vercel.app/" target="blank" >Admin Panel</a></p>
+           </> 
+          
+          :null
+          
+          
         }
         
         <p className='ml-10'> { hastoken ?  <button onClick={handleLogout}>Logout</button> : <Link to='/login'  className=' hover:text-slate-300' onClick={handleLogout}> LOGIN</Link>} </p>
